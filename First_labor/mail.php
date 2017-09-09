6a2d8ef87d09c0e6872ac3a923e590da45b41e18
asuvorkov <andrey.suvorkov@gmail.com>
2017-09-09 13:38:35 +0200

    mail.php - small layout changes

--- First_labor/mail.php
+++ First_labor/mail.php
@@ -2,10 +2,10 @@
 $phone = $_REQUEST['phone'];
 $name = $_REQUEST['name'];
 $report = $_REQUEST['report'];
-$message = "Tel: " . $phone . "\nName: " . $name . "\nMessage: " . $report;
+$message = "Tel: " . $phone . "\n\nName: " . $name . "\n\nMessage: " . $report;
 
 
-mail("rabota@1trudovoy.ru", "Obratnii Zvonok" , $message , "From: rabota@1trudovoy.ru");
+mail("rabota@1trudovoy.ru", "Call back" , $message , "From: rabota@1trudovoy.ru");
 
 
 echo "Ваша заявка принята. Менеджер свяжется с Вами в ближайшее время.";
