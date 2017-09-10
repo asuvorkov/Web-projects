470e52a4c3cc49225137f31ffb75d11958509260
asuvorkov <andrey.suvorkov@gmail.com>
2017-09-10 17:32:54 +0200

    index.html - form layout change, modal dialog layout change
    mail.php - small fixes
    
    vacancy.html - salary change

--- First_labor/mail.php
+++ First_labor/mail.php
@@ -4,10 +4,6 @@ $name = $_REQUEST['name'];
 $report = $_REQUEST['report'];
 $message = "Tel: " . $phone . "\n\nName: " . $name . "\n\nMessage: " . $report;
 
-
-mail("rabota@1trudovoy.ru", "Call back" , $message , "From: rabota@1trudovoy.ru");
-
-
-echo "Ваша заявка принята. Менеджер свяжется с Вами в ближайшее время.";
+mail("rabota@1trudovoy.ru", "Call back", $message, "From: rabota@1trudovoy.ru");
 
 ?>
