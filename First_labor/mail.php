f8152927ba41d186c44277d76fdac5538d3522e4
asuvorkov <andrey.suvorkov@gmail.com>
2017-09-12 15:09:21 +0200

    index.html - add a ajax request to mail.php via jQuery.js and AJAX, mail.php - small fixes, common - reduce of files(pictures) which are not used.

--- First_labor/mail.php
+++ First_labor/mail.php
@@ -2,8 +2,9 @@
 $phone = $_REQUEST['phone'];
 $name = $_REQUEST['name'];
 $report = $_REQUEST['report'];
+
 $message = "Tel: " . $phone . "\n\nName: " . $name . "\n\nMessage: " . $report;
 
-mail("rabota@1trudovoy.ru", "Call back", $message, "From: rabota@1trudovoy.ru");
+mail("Rabota@uvcentre.ru", "Call back", $message, "From: Rabota@uvcentre.ru");
 
 ?>
