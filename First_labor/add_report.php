8983cc6a6fb437a15b21ccdae95f1910225a86bd
asuvorkov <andrey.suvorkov@gmail.com>
2017-10-08 17:27:15 +0200

    Add avito social, add a dynamical upload of reports, layout fixes, small fixes

---
+++ First_labor/add_report.php
@@ -0,0 +1,14 @@
+<?php
+$current_data = file_get_contents('reports.json');
+
+$array_data   = json_decode($current_data, true);
+
+$extra_data = array(
+    'name' => $_POST["name"],
+    'date' => $_POST["date"],
+    'message' => $_POST["message"]
+);
+array_unshift($array_data[0], $extra_data);
+$final_data = json_encode($array_data);
+file_put_contents( 'reports.json', $final_data );
+?>
