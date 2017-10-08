37790ecbbfcebb2fdc4a4de8272a94a6a30975e5
asuvorkov <andrey.suvorkov@gmail.com>
2017-10-08 18:51:52 +0200

    add_report.php - fix a bug with a date bz adding a report

--- First_labor/add_report.php
+++ First_labor/add_report.php
@@ -3,9 +3,11 @@ $current_data = file_get_contents('reports.json');
 
 $array_data   = json_decode($current_data, true);
 
+$date = date("d.m.Y");
+
 $extra_data = array(
     'name' => $_POST["name"],
-    'date' => $_POST["date"],
+    'date' => $date,
     'message' => $_POST["message"]
 );
 array_unshift($array_data['reports'], $extra_data);
