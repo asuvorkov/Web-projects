d61b20acd59f0c34cec1f6cab986bb3f141fded2
asuvorkov <andrey.suvorkov@gmail.com>
2017-11-24 15:17:50 +0100

    Andrei Suvorkov - portfolio page - improvement of a projects block layout, optimisation JS code

--- Andrei_Suvorkov/js/portfolio.js
+++ Andrei_Suvorkov/js/portfolio.js
@@ -47,41 +47,10 @@
     }
 
     /* portfolio projects animation */
-    $('.project_wrapper_1').hide();
-    $('.project_1').hover(function() {
-        $('.project_wrapper_1').toggle();
+    $('.project_wrapper').hide();
+    $('.project').hover(function() {
+        $(this).find('.project_wrapper').toggle();
     });
-    $('.project_wrapper_2').hide();
-    $('.project_2').hover(function() {
-        $('.project_wrapper_2').toggle();
-    });
-    $('.project_wrapper_3').hide();
-    $('.project_3').hover(function() {
-        $('.project_wrapper_3').toggle();
-    });
-    $('.project_wrapper_4').hide();
-    $('.project_4').hover(function() {
-        $('.project_wrapper_4').toggle();
-    });
-    $('.project_wrapper_5').hide();
-    $('.project_5').hover(function() {
-        $('.project_wrapper_5').toggle();
-    });
-    $('.project_wrapper_6').hide();
-    $('.project_6').hover(function() {
-        $('.project_wrapper_6').toggle();
-    });
-    $('.project_wrapper_7').hide();
-    $('.project_7').hover(function() {
-        $('.project_wrapper_7').toggle();
-    });
-    $('.project_wrapper_8').hide();
-    $('.project_8').hover(function() {
-        $('.project_wrapper_8').toggle();
-    });
-    $('.project_wrapper_9').hide();
-    $('.project_9').hover(function() {
-        $('.project_wrapper_9').toggle();
-    });
+
 
 })();
